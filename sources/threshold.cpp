f60c5b322896af49bdfff5e7f03bd78616958c19
kuanna <annak94@mail.ru>
2015-08-29 23:59:40 +0300

    Adding function for circles and ellipses. Fixed. Function draw()

--- sources/threshold.cpp
+++ sources/threshold.cpp
@@ -15,11 +15,11 @@ public:
     {
         int thresh = 100;
         int max_thresh = 255;
-        RNG rng(12345);
-        int counter = 0;
-
-        Mat gray_image;
+		Mat gray_image;
+		
         imshow( " ", image );
+		
+		//Resize and flip
         int size_height = image.size().height;
         int size_width = image.size().width;
         if (size_height > size_width )
@@ -29,33 +29,33 @@ public:
             size_height = image.size().height;
             size_width = image.size().width;
         }
-
         if (size_width > 640 )
         {
-            Mat scaled_image;
-            resize( image, scaled_image, Size(640,480) );
-            cvtColor( scaled_image, gray_image, CV_BGR2GRAY );
+            resize( image, image, Size(640,480) );
+            cvtColor( image, gray_image, CV_BGR2GRAY );
         }
         else
+		{
             cvtColor( image, gray_image, CV_BGR2GRAY );
+		}
+		
         /// Reduce the noise so we avoid false circle detection
         GaussianBlur( gray_image, gray_image, Size(9, 9), 2, 2 );
+		
+		/// Filling empty spots with Morph 
         Mat kernel;
         kernel = Mat::ones( 6, 6, CV_8UC1);
         kernel = kernel * 255;
         morphologyEx(gray_image, gray_image, MORPH_OPEN, kernel);
-        Mat threshold_output;
-        vector<vector<Point> > contours;
-        vector<Vec4i> hierarchy;
-
-        /// Detect edges using Threshold
+		
+		/// Detect edges using Threshold
+		Mat threshold_output;
         threshold( gray_image, threshold_output, thresh, 255, THRESH_OTSU);
-
-        /// Filling empty spots with Morph 
-
-        imshow("tre", threshold_output);
+        imshow("Threshold", threshold_output);
 
         /// Find contours
+		vector<vector<Point> > contours;
+        vector<Vec4i> hierarchy;
         findContours( threshold_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
 
         /// Find the rotated rectangles and ellipses for each contour
@@ -69,29 +69,27 @@ public:
                 if ( ( area < 57600 ) && ( area > 300 ) )
                 {
                       minEllipse.push_back(fitEllipse( Mat(contours[i])));
-                      counter++ ;
                 }
             }
         }
 
-        /// Draw contours + rotated rects + ellipses
-        Mat drawing = Mat::zeros( threshold_output.size(), CV_8UC3 );
-        for( int i = 0; i< contours.size(); i++ )
-        {
-             Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-             // contour
-             drawContours( drawing, contours, i, color, 1, 8, vector<Vec4i>(), 0, Point() );
-              // ellipse
-            ellipse( drawing, minEllipse[i], color, 2, 8 );
-        }
-        imshow("Threshold", drawing  );
-        std::cout<< "number of coins:\t" << counter << std::endl;
-        waitKey(0);
-        return counter; 
+        return minEllipse.size(); 
     }
 
     void draw()
-    {}
+    { 
+		RNG rng(12345);
+		/// Draw ellipses
+        for( int i = 0; i < minEllipse.size(); i++ )
+        {
+			Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+            // ellipse
+			ellipse( image, minEllipse[i], color, 2, 8 );
+        }
+        imshow("Ellipses", image);
+        std::cout<< "number of coins:\t" << minEllipse.size() << std::endl;
+        waitKey(0);
+	}
     
     cv::vector<cv::Vec3f> write_circles()
 	{
